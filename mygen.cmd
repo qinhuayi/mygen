413696599be3e313025384d03aaeae06d2c0fd0f
qinhuayi <qinhuayi@qq.com>
2021-06-28 23:02:09 +0800

    修改脚本中的测试用数据库连接字符串

--- mygen.cmd
+++ mygen.cmd
@@ -1,5 +1,5 @@
-set CONN="127.0.0.1:3306|qinhy|123456|db_entbase|tb_workflow"
+set CONN="127.0.0.1:3306|vault-keeper|123456|db_vault|tb_user"
 set TMPL=templates/entity.kt
-set OUT=output/entity.kt
+set OUT=output/User.kt
 set CONSTS="author=qinhuayi|email=qinhuayi@qq.com"
 python mygen.py %CONN% "%TMPL%" "%OUT%" %CONSTS%
--- mygen.sh
+++ mygen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CONN="127.0.0.1:3306|qinhy|123456|db_entbase|tb_workflow"
+CONN="127.0.0.1:3306|vault-keeper|123456|db_vault|tb_user"
 TMPL="templates/entity.kt"
-OUT="output/entity.kt"
+OUT="output/User.kt"
 CONSTS="author=qinhuayi|email=qinhuayi@qq.com"
 python mygen.py $CONN $TMPL $OUT $CONSTS
