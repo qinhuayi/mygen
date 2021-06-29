c42ad995d521effc76916365c07e4c89479560fc
qinhuayi <qinhuayi@qq.com>
2021-06-29 14:44:14 +0800

    移除无用的JSONEncoder.py

--- JSONEncoder.py
+++
@@ -1,8 +0,0 @@
-﻿import json
-class JSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, bytes):
-            return str(obj, encoding='utf-8')
-        if isinstance(obj, datetime.date):
-            return obj.strftime('%Y/%m/%d')
-        return json.JSONEncoder.default(self, obj)
