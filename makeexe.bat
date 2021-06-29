1ab6fcd54c8eb779860f39c090f4f9e91b093d29
qinhuayi <qinhuayi@qq.com>
2021-06-29 16:08:47 +0800

    移除一处个人用的xcopy命令

--- makeexe.bat
+++ makeexe.bat
@@ -1,7 +1,6 @@
 set DIR=%~dp0
 cd /d "%DIR%"
 pyinstaller --version-file=version.txt -F mygen.py 
-xcopy dist/mygen.ex* "D:/Projects/Dongxin/Kaibang/mygen/" /I /Y /D /U
 rd /S /Q __pycache__
 rd /S /Q build
 rd /S /Q dist\build
