e8121ea0230cb7979deb3a89a2729032dfdb9e4d
qinhuayi <qinhuayi@qq.com>
2021-06-30 00:22:14 +0800

    调试编辑window和linux下的shell脚本

--- mygen.cmd
+++ mygen.cmd
@@ -1,5 +1,7 @@
+@echo off
 set CONN="127.0.0.1:3306|vault-keeper|123456|db_vault|tb_user"
-set TMPL=templates/entity.kt
-set OUT=output/User.kt
+set TMPL=./templates/entity.java
+set OUT=./output/User.java
 set CONSTS="author=qinhuayi|email=qinhuayi@qq.com"
+@echo on
 python mygen.py %CONN% "%TMPL%" "%OUT%" %CONSTS%
--- mygen.sh
+++ mygen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONN="127.0.0.1:3306|vault-keeper|123456|db_vault|tb_user"
-TMPL="templates/entity.kt"
-OUT="output/User.kt"
+TMPL="templates/entity.java"
+OUT="output/User.java"
 CONSTS="author=qinhuayi|email=qinhuayi@qq.com"
 python mygen.py $CONN $TMPL $OUT $CONSTS
