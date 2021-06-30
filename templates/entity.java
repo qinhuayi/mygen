542af570c7a6b58175f97fa833c7c1fb7d357548
qinhuayi <qinhuayi@qq.com>
2021-06-30 22:46:46 +0800

    1 修复一个单词的拼写错误; 2 用原生语法替换一段简洁语法.

--- templates/entity.java
+++ templates/entity.java
@@ -1,9 +1,22 @@
 ﻿package pers.qinhuayi.vault.entity;
-
+<%
+var removePrefix = function (name) {
+	return name.replace(/^(tb_|v_|vw_|s_|sys_)/, '');
+},
+	pascal = function (name) {
+		return removePrefix(name).replace(/(?:^|_)([a-z])/g, function($0,$1){ 
+			return $1.toUpperCase() 
+		});
+	},
+	camel = function (name) {
+		return removePrefix(name).replace(/_([a-z])/g, function($0,$1){ 
+			return $1.toUpperCase() 
+		}).replace(/^[A-Z]/, function($0){ 
+			return $0.toLowerCase() 
+		});
+	};
+%>
 import pers.qinhuayi.framework.entity.Table;
-{{set removePrefix = function(name){ return name.replace(/^(tb_|v_|vw_|s_|sys_)/, '')}; }}
-{{set pascal = function(name){ return removePrefix(name).replace(/(?:^|_)([a-z])/g, function($0,$1){ return $1.toUpperCase() } )}; }}
-{{set camal = function(name){ return removePrefix(name).replace(/_([a-z])/g, function($0,$1){ return $1.toUpperCase() } ).replace(/^[A-Z]/, function($0){ return $0.toLowerCase() }) }; }}
 /**
  * {{@table.comment}}
  * @author {{@consts.author}} 
@@ -13,5 +26,5 @@ import pers.qinhuayi.framework.entity.Table;
 public record {{@pascal(table.name)}} ({{each columns as col i}}
 	//{{@col.COLUMN_COMMENT}}
 	@Table.Column("{{@col.COLUMN_NAME}}")
-	{{@col.javatype}} {{@camal(col.COLUMN_NAME)}}{{if i < columns.length - 1}},{{/if}} {{/each}}
+	{{@col.javatype}} {{@camel(col.COLUMN_NAME)}}{{if i < columns.length - 1}},{{/if}} {{/each}}
 ) 
--- templates/entity.kt
+++ templates/entity.kt
@@ -1,13 +1,13 @@
 ﻿package com.kezhida.webserver.entity
 {{set removePrefix = function(name){ return name.replace(/^(tb_|v_|vw_|s_|sys_)/, '')}; }}
 {{set pascal = function(name){ return removePrefix(name).replace(/(?:^|_)([a-z])/g, function($0,$1){ return $1.toUpperCase() } )}; }}
-{{set camal = function(name){ return removePrefix(name).replace(/_([a-z])/g, function($0,$1){ return $1.toUpperCase() } ).replace(/^[A-Z]/, function($0){ return $0.toLowerCase() }) }; }}
+{{set camel = function(name){ return removePrefix(name).replace(/_([a-z])/g, function($0,$1){ return $1.toUpperCase() } ).replace(/^[A-Z]/, function($0){ return $0.toLowerCase() }) }; }}
 /**
  * @author {{@consts.author}} 
  * @email {{@consts.email}}
  * {{@table.comment}}
  */ 
 public data class {{@pascal(table.name)}} (
-	{{each columns as col i}}var {{@camal(col.COLUMN_NAME)}}: {{@col.javatype}}{{if i < columns.length - 1}},{{/if}} //{{@col.COLUMN_COMMENT}}
+	{{each columns as col i}}var {{@camel(col.COLUMN_NAME)}}: {{@col.javatype}}{{if i < columns.length - 1}},{{/if}} //{{@col.COLUMN_COMMENT}}
 	{{/each}}
 )
